b9b46e7596483b22dbe891d80cd7e7cec8a0e0af
milekpl <milek_pl@o2.pl>
2026-02-21 19:36:54 +0100

    fix Jest configuration for Github workflow

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testMatch: ['<rootDir>/tests/**/*.test.js'],
   testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
   transform: {
     '^.+\\.jsx?$': ['babel-jest', { configFile: false, presets: ['@babel/preset-env'] }],
   },
